d713e1a1f273c42bf44ed745dee7b2773bee006a
Wiston0004 <wistonchiang04@gmail.com>
2025-09-09 08:55:49 +0800

    update user manager

--- resources/views/dashboards/manager.blade.php
+++ resources/views/dashboards/manager.blade.php
@@ -5,19 +5,19 @@
   <div class="card center">
     <h3 style="margin-top:0">Manage Users</h3>
     <p class="muted">Create and update Staff & Customers</p>
-    <a class="btn primary" href="{{ route('manager.users.index') }}">Open User Manager</a>
+    <a class="btn primary" href="{{ route('manager.users.index') }}">User Management</a>
   </div>
 
   <div class="card center">
     <h3 style="margin-top:0">Reports</h3>
     <p class="muted">See monthly customer registrations</p>
-    <a class="btn" href="{{ route('manager.reports.customers') }}">View Reports</a>
+    <a class="btn primary" href="{{ route('manager.reports.customers') }}">View Reports</a>
   </div>
 
   <div class="card center">
     <h3 style="margin-top:0">Transactions</h3>
     <p class="muted">All payments & refunds</p>
-    <a class="btn" href="{{ route('manager.transactions.index') }}">View Transactions</a>
+    <a class="btn primary" href="{{ route('manager.transactions.index') }}">View Transactions</a>
   </div>
 
     <div class="card center">
